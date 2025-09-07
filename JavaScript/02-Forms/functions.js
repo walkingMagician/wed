da4285cb3e6da76e19415b207b5fd6ccbc25fd80
I'm kotэ <egorsulga0@gmail.com>
2025-09-07 12:00:40 +0300

    done

--- JavaScript/02-Forms/functions.js
+++ JavaScript/02-Forms/functions.js
@@ -16,18 +16,38 @@ function GetMouseCoords(event)
 function SwitchBackground()
 {
     let SwitchBackground = document.getElementById("switch-background");
-    /*if (SwitchBackground.style.backgroundImage === 'url("Image/moon.png")')
-    {
-        SwitchBackground.style.backgroundImage = 'url("Image/sun.png")';
-        document.body.className = "dark";
-    }
-    else
-        {
-            SwitchBackground.style.backgroundImage = 'url("Image/moon.png")';
-            document.body.className = "light";
-    }*/
     let delay = document.getElementById('delay').value;
     document.body.style.transition = `background-color ${delay}s`, `color ${delay}s`;
     document.getElementById('switch-background').style.transition = `background-image ${delay}s`;
     document.body.className = document.body.className === "light" ? "dark" : "light";
+}
+
+function UploadPhoto(input)
+{
+    let photo = document.getElementById("photo");
+    
+    if (input.files && input.files[0]) {
+        let reader = new FileReader();
+        
+        reader.onload = function(e) {
+            photo.src = e.target.result;
+            photo.alt = "Фото студента";
+            photo.title = "Фото студента";
+        }
+        
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+function ToggleForm(formId) {
+    const form = document.getElementById(formId);
+    const toggleIcon = form.previousElementSibling.querySelector('.toggle-icon');
+    
+    form.classList.toggle('collapsed');
+    
+    if (form.classList.contains('collapsed')) {
+        toggleIcon.textContent = '+';
+    } else {
+        toggleIcon.textContent = '−';
+    }
 }
